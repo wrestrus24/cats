14741e21cfc23250b7b25720f987dcdfe509860e
wrestrus24 <varfolomee999777@gmail.com>
2024-11-13 10:22:11 +0700

    Создано окно и виджеты

--- CATS.py
+++ CATS.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window=Tk()
+window=title("Cats!")
+window.geometry("500x500")
+
+label=Label()
+label.pack()
+
+url= "https://cataas.com/cat"
+img= load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image=img
+
+window.mainloop()
