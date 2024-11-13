6e9b7e2442581e3bcd8214ce24301e04a4680d27
wrestrus24 <varfolomee999777@gmail.com>
2024-11-13 10:40:35 +0700

    Ограничили размер картинки

--- CATS.py
+++ CATS.py
@@ -11,6 +11,7 @@ def load_image(url):
         response.raise_for_status()
         image_data= BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"ERROR: {e}")
@@ -24,7 +25,7 @@ def set_image():
 
 window=Tk()
 window.title("Cats!")
-window.geometry("500x500")
+window.geometry("600x600")
 
 label=Label()
 label.pack()
@@ -37,5 +38,7 @@ if img:
     label.image=img
 update_button=Button(text="Обновить", command=set_image)
 update_button.pack()
+
 set_image()
+
 window.mainloop()
