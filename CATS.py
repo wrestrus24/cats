8feadc75b51e1123e2e3c29f0e24bc3919e410c1
wrestrus24 <varfolomee999777@gmail.com>
2024-11-13 10:26:27 +0700

    Создана функция для полученя изображения

--- CATS.py
+++ CATS.py
@@ -3,6 +3,20 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+
+def load_image():
+    try:
+        response= requests.get(url)
+        response.raise_for_status()
+        image_data= BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"ERROR: {e}")
+        return None
+
+
 window=Tk()
 window=title("Cats!")
 window.geometry("500x500")
